360bf140015776ee3f0189e64cee27ff68cdb33e
Toshiro-Morishita <toshimori1234@gmail.com>
2019-02-05 16:06:26 +0900

    finish TresureHunter

--- js/main.js
+++ js/main.js
@@ -14,11 +14,15 @@
     for (i = 0; i < boxes.length; i++) {
       boxes[i].addEventListener('click', function() {
         let j;
+        if (this.className.indexOf('shake') === -1) {
+          return;
+        }
         for (j = 0; j < boxes.length; j++) {
           boxes[j].src = 'img/' + contents[Math.floor(Math.random() * contents.length)];
           boxes[j].className = 'box disabled';
         }
         this.className = 'box';
+        document.getElementById('btn').className = '';
       });
     }
   }
