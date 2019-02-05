ffff2d6eb749bd9e9647fa54454a86cb5ba5aeaa
Toshiro-Morishita <toshimori1234@gmail.com>
2019-02-05 15:35:23 +0900

    add random-image

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@
 
   let boxes = document.getElementsByClassName('box');
 
+  let contents = [
+    'coin.png',
+    'empty.png',
+    'cobra.png'
+  ];
+
   function init() {
     let i;
     for (i = 0; i < boxes.length; i++) {
       boxes[i].addEventListener('click', function() {
-        this.src = 'img/coin.png';
+        this.src = 'img/' + contents[Math.floor(Math.random() * contents.length)];
         this.className = 'box';
       });
     }
